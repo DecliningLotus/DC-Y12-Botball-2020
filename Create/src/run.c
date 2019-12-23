927dfdd93e936d6a9995b6add4d0f4fcfdf29309
Lotus <38220115+LotusJS@users.noreply.github.com>
2019-12-23 20:52:00 +1000

    Quick fix

--- Create/src/run.c
+++ Create/src/run.c
@@ -4,19 +4,41 @@
 
 void run()
 {
-    //Instruction path here. The following are examples just so people aren't confused.
+    //Begin moving Create towards first point in front of Communication Orb.
+    moveForwardDistance(100, <value>);
+    spinLeftAngle(90);
 
-    //Drive until bump with digital sensor example.
-    while (digital(0) == 0)
+    //Activate crane to pick up Communication Orb.
+    //Crane mechanics TBD
+
+    //Spin spinners to correct colors.
+    //Spinner mechnaics TBD.
+
+    //Begin moving towards second point in front of Mountain View.
+    spinRightAngle(180);
+    while (analog(<distance sensor port>) <= <analog value>)
     {
+        //Distance sensor measuring distance from Minecart ahead, until it should stop.
         moveForward(100);
     }
     moveStop();
 
-    //Drive until analog sensor threshold is met.
-    while (analog(0) <= 1500)
-    {
-        moveBackward(75);
-    }
-    moveStop();
+    spinLeftAngle(90);
+    moveForwardDistance(100, <value>);
+
+    //Activate crane to pick up Botguy and place Communication Orb.
+    //Crane mechanics TBD
+
+    //Begin sweep of leftover puffles.
+    spinRightAngle(180);
+    moveForwardDistance(100, <value>);
+    //Sweep mechanics TBG
+
+    //Return to spot in front of puffle stockpiles.
+    spinLeftAngle(100, 45);
+    moveForwardDistance(100, <value>);
+    spinLeftAngle(100, 45);
+
+    //Begin sorting process.
+    //Sorting mechanics TBD.
 }
