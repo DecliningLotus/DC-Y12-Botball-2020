4b94a3aa7406a9a0745435caf0a2d4bcca37762a
Lotus <38220115+LotusJS@users.noreply.github.com>
2020-01-12 09:11:46 +0300

    Updated commented instruction set in run.c

--- Lego/src/run.c
+++ Lego/src/run.c
@@ -4,60 +4,15 @@
 
 void run()
 {
-    //Begin Lego movement down the board sweeping puffles.
-    int i;
-    for (size_t i = 0; i < 4; i++)
-    {
-        //Movement for first 4 Minecarts
-        while (analog(<port>) >= <value>)
-        {
-            //IR Reflectance sensor checking if there is minecart or not.
-            moveForward(1000);
-        }
-        moveStop();
+    //Move towards Material Transport.
 
-        //Sweep puffles.
-        //Sweeping mechanics TBD.
-    }
+    //Pull Material Transport out.
 
-    //Cross underneath bridge.
-    moveForwardDistance(900, <distance ticks>);
+    //Bring Material Transport underneath bridge.
 
-    //Continue with last 3 Minecarts.
-    for (size_t i = 0; i < 3; i++)
-    {
-        while (analog(<port>) >= <value>)
-        {
-            //IR Reflectance sensor checking if there is minecart or not.
-            moveForward(1000);
-        }
-        moveStop();
+    //Use claw to bring Royal Blue Puffles down into Material Transport.
 
-        //Sweep puffles.
-        //Sweeping mechanics TBD.
-    }
+    //Push Material Transport into Processing Zone.
 
-    //Begin reversing to starting position.
-    moveBackwardDistance(1499, <distance>);
-    while ((digital(<port1>) == 0) && (digital(<port2>) == 0)) //Slow down bot before hitting the PVC pipe to ensure we don't crash it hard.
-    {
-        moveBackward(500);
-    }
-    moveStop();
-
-    //Head in front of Material Transport.
-    moveForwardDistance(400, 600); //Ease the bot out so it has turning space.
-    spinRight(300, <time>);
-
-    moveForwardDistance(1500, <distance>);
-    while ((digital(<port1>) == 0) && (digital(<port2>) == 0)) //Slow down bot before hitting PVC pipe.
-    {
-        moveForward(500);
-    }
-    moveStop();
-
-    //Knock out Material Transport
-    //Mechanics TBD
-
-    //Going up the ramp etc will be discussed after Material Transport is rseolved.
+    //Return to Ramp and go up to park.
 }
