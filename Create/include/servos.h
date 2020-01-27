da1ad669a411aa75a68ca3db0b16b3e3e9b7930b
Lotus <38220115+LotusJS@users.noreply.github.com>
2020-01-27 13:12:58 +0300

    updated crane system

--- Create/include/servos.h
+++ Create/include/servos.h
@@ -1,10 +1,8 @@
 #include <kipr/botball.h>
 
-#define SERVO_1_PORT <port number>
-#define SERVO_2_PORT <port number>
-#define MOTOR_1_PORT <port number>
-#define MOTOR_2_PORT <port number>
-#define DISTANCE_SENSOR_PORT <port number>
+#define ARM_PORT 1
+#define CLAW_PORT 2
+#define MOTOR_1_PORT 0
 
 void raiseArmTop();
 
