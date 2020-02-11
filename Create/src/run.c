984641dbb5b2c712d3eb12b01eeabff8ff90a635
Lotus <38220115+LotusJS@users.noreply.github.com>
2020-02-11 15:35:43 +0300

    updated run instructions + refactored most of servos.c with better tested values

--- Create/src/run.c
+++ Create/src/run.c
@@ -4,66 +4,46 @@
 
 void run()
 {
-    //    unfoldArm();
-    //    msleep(5000);
+    //unfoldArm();
+    //msleep(300);
 
     //Charge and Grab BotGuy
-    msleep(3000);
-    openClaw(2000);
-    spinLeftAngle(100, 46);
+    openClaw();
+    spinLeftAngle(100, 18);
     moveForwardDistance(300, 800);
     while (get_create_rbump() == 0)
     {
-        moveForward(150);
+        moveForward(200);
     }
     moveStop();
-    closeClaw(2000);
+    closeClaw();
+    msleep(200);
 
     //Drop BotGuy
-    moveBackwardDistance(100, 215);
-    spinLeftAngle(100, 40);
+    moveBackwardDistance(100, 300);
+    spinRightAngle(60, 90);
+    openClaw();
+    msleep(500);
+    spinLeftAngle(60, 90);
 
     //Set pos for communication Orb
-    motor(MOTOR_1_PORT, 50);
-    msleep(5000);
-    motor(MOTOR_1_PORT, 0);
-    set_servo_position(ARM_PORT, 250);
-    msleep(500);
-    spinLeftAngle(100, 90);
-    openClaw(1800);
+    spinLeftAngle(100, 141);
+    lowerArmBottom();
 
     // Go to communication orb
-    moveBackwardDistance(200, 100);
-    msleep(1000);
-    spinLeftAngle(100, 40);
-
-    //Shove Ore Boxes to Storage
-    //Do ore box stuff
+    moveForwardDistance(300, 230);
+    msleep(100);
 
     //Pick up Communication Orb
-    openClaw();
-    lowerArmBottom();
     closeClaw();
     raiseArmTop();
 
     //Head towards Communication Orb staging zone
-    spinLeftAngle(250, 182);
-    moveForwardDistance(450, 500);
-    spinLeftAngle(250, 47);
-    while (analog(IR_PORT) < 500)
+    spinRightAngle(100, 141);
+    moveForwardDistance(300, 250);
+    while (get_create_rbump() == 0)
     {
         moveForward(300);
     }
     moveStop();
-
-    //Place communication and HOLD
-    raiseArmTop();
-    while (analog(IR_PORT) < 300)
-    {
-        moveForward(500);
-    }
-    moveStop();
-
-    //Pull up Minecart with second crane
-    //Mechanics TBD
 }
--- Create/src/servos.c
+++ Create/src/servos.c
@@ -5,55 +5,60 @@ int i;
 void unfoldArm()
 {
     motor(MOTOR_1_PORT, -50);
-    msleep(4000);
+    msleep(4900);
     motor(MOTOR_1_PORT, 0);
-    for (i = 1900; i >= 325; i -= 15)
+    for (i = 1900; i >= 500; i -= 15)
     {
         set_servo_position(ARM_PORT, i);
         msleep(15);
     }
-    motor(MOTOR_1_PORT, -50);
-    msleep(1500);
-    motor(MOTOR_1_PORT, 0);
+    set_servo_position(ARM_PORT, 500);
 }
 
 void raiseArmTop()
 {
-    for (i = 1800; i >= 325; i -= 15)
+    motor(MOTOR_1_PORT, -50);
+    msleep(4000);
+    motor(MOTOR_1_PORT, 0);
+    msleep(100);
+    for (i = 180; i <= 500; i += 7)
     {
         set_servo_position(ARM_PORT, i);
         msleep(15);
     }
-    set_servo_position(ARM_PORT, 360);
-    msleep(100);
-    motor(MOTOR_1_PORT, 300);
-    motor(MOTOR_1_PORT, 0);
+    set_servo_position(ARM_PORT, 500);
 }
 
 void lowerArmBottom()
 {
-    for (i = 360; i <= 700; i += 15)
+    for (i = 500; i >= 180; i -= 15)
     {
         set_servo_position(ARM_PORT, i);
         msleep(15);
     }
-    set_servo_position(ARM_PORT, 700);
+    set_servo_position(ARM_PORT, 180);
     msleep(100);
-    motor(MOTOR_1_PORT, -50);
-    msleep(1000);
+    motor(MOTOR_1_PORT, 50);
+    msleep(4000);
     motor(MOTOR_1_PORT, 0);
 }
 
-void openClaw(time)
+void openClaw()
 {
-    motor(CLAW_PORT, -50);
-    msleep(time);
-    motor(CLAW_PORT, 0);
+    for (i = 500; i <= 1300; i += 30)
+    {
+        set_servo_position(CLAW_PORT, i);
+        msleep(15);
+    }
+    set_servo_position(CLAW_PORT, 1300);
 }
 
-void closeClaw(time)
+void closeClaw()
 {
-    motor(CLAW_PORT, 50);
-    msleep(time);
-    motor(CLAW_PORT, 0);
+    for (i = 1300; i >= 500; i -= 30)
+    {
+        set_servo_position(CLAW_PORT, i);
+        msleep(15);
+    }
+    set_servo_position(CLAW_PORT, 500);
 }
